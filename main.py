86e849ffdd6e14be6db23ca8614af28e9ac47266
Icehunter494 <149531493+Icehunter494@users.noreply.github.com>
2026-05-13 02:53:17 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from fastapi import FastAPI, UploadFile, File
-import shutil
-from audio import load_wav
-from pitch import analyze_audio
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://your-vercel-app.vercel.app"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app = FastAPI()
 
