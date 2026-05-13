64b7c8415e3b735d448747b468ce2b66e611f612
Icehunter494 <149531493+Icehunter494@users.noreply.github.com>
2026-05-13 14:01:15 -0400

    Update audio.py

--- audio.py
+++ audio.py
@@ -1,15 +1,18 @@
 import wave
-import struct
+import numpy as np
 
 def load_wav(filename):
     with wave.open(filename, 'rb') as w:
         frames = w.readframes(w.getnframes())
 
-        samples = struct.unpack(
-            "<" + str(w.getnframes()) + "h",
-            frames
-        )
-
         sr = w.getframerate()
+        channels = w.getnchannels()
 
-        return samples, sr
+        # convert to int16 safely
+        audio = np.frombuffer(frames, dtype=np.int16)
+
+        # convert stereo → mono if needed
+        if channels == 2:
+            audio = audio.reshape(-1, 2).mean(axis=1).astype(np.int16)
+
+        return audio, sr
